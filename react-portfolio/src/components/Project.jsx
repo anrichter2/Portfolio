6b1d3cc17b400231897bff71a673d44a651c1bd3
anrichter <anrichter2@gmail.com>
2024-09-15 22:25:13 -0500

    Filled out the Portfolio.jsx and Project.jsx files with example data so that I can get the portfolio page to render the way that I want it to when I have the real data ready. Also created a project.css file so that I can style the rendered projects. Still some bugs with the layout so will have to return later to fix them.

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,6 +1,23 @@
+import './Project.css'
 
-export default function Project() {
+export default function Project({project}) {
+
+    // use .map to display all of the project info
+    // will need an image from the project, url for deployed applicationa and github repository
     return (
-        <div></div>
+        <div className='col-md-6 my-2 py-2'>
+            <div className="image-container">
+                <img className='project-image' src="https://placehold.co/600x400" alt="exampleImage" />
+                <div className='url-div'>
+                    {/* make it so that these a tags are in a flex row with col-6 */}
+                    <a className='my-2' href={project.gitHubURL}>
+                        <p className="image-text my-2">{project.projectTitle}</p>
+                    </a>
+                    <a className='my-2' href={project.deployedURL}>
+                        <img className="url-link" src="https://placehold.co/125x75" />
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 };
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -1,6 +1,56 @@
+import Project from "../components/Project";
 
 export default function Portfolio() {
+
+    // Add data of projects for the project page tehn pass to project component
+    // array of objects with title:, Github URL:, deployed URL:
+    const projects = [{
+        id: 1,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    },
+    {
+        id: 2,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    },
+    {
+        id: 3,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    },
+    {
+        id: 4,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    },
+    {
+        id: 5,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    },
+    {
+        id: 6,
+        projectTitle: 'Example',
+        gitHubURL: 'https://github.com/anrichter2',
+        deployedURL: 'https://www.google.com'
+    }];
+
     return (
-        <div></div>
+        <div>
+            <h2>Portfolio</h2>
+            <div className="container">
+                <div className="row">
+                {projects.map((project) => (
+                    <Project key={project.id} project={project}/>
+                ))}
+                </div>
+            </div>
+        </div>
     );
 };
